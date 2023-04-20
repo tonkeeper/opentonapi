f4cfc41a4f2729c9b4c676c00cfb24ab438fcb62
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2023-04-20 22:24:34 +0800

    add all fiats currencies to rates

--- pkg/api/interfaces.go
+++ pkg/api/interfaces.go
@@ -90,7 +90,8 @@ type addressBook interface {
 }
 
 type tonRates interface {
-	GetRates() map[string]string
+	GetRates() map[string]float64
+	GetPools() map[string]float64
 }
 
 type metadataCache struct {
