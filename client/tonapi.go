5b4ae4636b91476179274b20616f9f817183cb1b
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2023-08-08 16:24:08 +0700

    uncomment tongo connector

--- client/tonapi.go
+++ client/tonapi.go
@@ -1,56 +1,55 @@
 package client
 
-//
-//import (
-//	"context"
-//	"encoding/base64"
-//	"github.com/tonkeeper/tongo"
-//	"github.com/tonkeeper/tongo/tlb"
-//)
-//
-//func (c *Client) GetSeqno(ctx context.Context, account tongo.AccountID) (uint32, error) {
-//	res, err := c.GetAccountSeqno(ctx, GetAccountSeqnoParams{AccountID: account.ToRaw()})
-//	if err != nil {
-//		return 0, err
-//	}
-//	return res.Seqno, nil
-//}
-//
-//func (c *Client) SendMessage(ctx context.Context, payload []byte) (uint32, error) {
-//	err := c.SendMessageTemp(ctx, &SendMessageTempReq{Boc: base64.StdEncoding.EncodeToString(payload)})
-//	if err != nil {
-//		return 0, err
-//	}
-//	return 0, nil
-//}
-//
-//func (c *Client) GetAccountState(ctx context.Context, accountID tongo.AccountID) (tlb.ShardAccount, error) {
-//	s, err := c.GetRawAccount(ctx, GetRawAccountParams{AccountID: accountID.ToRaw()})
-//	if err != nil {
-//		return tlb.ShardAccount{}, err
-//	}
-//	var a tlb.ShardAccount
-//
-//	a.LastTransLt = uint64(s.LastTransactionLt)
-//	switch s.Status {
-//	case "nonexist":
-//		a.Account.SumType = "AccountNone"
-//	case "uninit":
-//		a.Account.SumType = "Account"
-//		a.Account.Account.Addr = accountID.ToMsgAddress()
-//		a.Account.Account.Storage.Balance.Grams = tlb.Grams(s.Balance)
-//		a.Account.Account.Storage.State.SumType = "AccountUninit"
-//	case "active":
-//		a.Account.SumType = "Account"
-//		a.Account.Account.Addr = accountID.ToMsgAddress()
-//		a.Account.Account.Storage.Balance.Grams = tlb.Grams(s.Balance)
-//		a.Account.Account.Storage.State.SumType = "AccountActive"
-//		a.Account.Account.Storage.State.AccountActive.StateInit.Code.Exists = len(s.Code.Value) > 0
-//		a.Account.Account.Storage.State.AccountActive.StateInit.Data.Exists = len(s.Data.Value) > 0
-//
-//	case "frozen":
-//
-//	}
-//	return a, nil
-//
-//}
+import (
+	"context"
+	"encoding/base64"
+
+	"github.com/tonkeeper/tongo"
+	"github.com/tonkeeper/tongo/tlb"
+)
+
+func (c *Client) GetSeqno(ctx context.Context, account tongo.AccountID) (uint32, error) {
+	res, err := c.GetAccountSeqno(ctx, GetAccountSeqnoParams{AccountID: account.ToRaw()})
+	if err != nil {
+		return 0, err
+	}
+	return res.Seqno, nil
+}
+
+func (c *Client) SendMessage(ctx context.Context, payload []byte) (uint32, error) {
+	var req SendBlockchainMessageReq
+	req.Boc.SetTo(base64.StdEncoding.EncodeToString(payload))
+	err := c.SendBlockchainMessage(ctx, &req)
+	if err != nil {
+		return 0, err
+	}
+	return 0, nil
+}
+
+func (c *Client) GetAccountState(ctx context.Context, accountID tongo.AccountID) (tlb.ShardAccount, error) {
+	res, err := c.GetBlockchainRawAccount(ctx, GetBlockchainRawAccountParams{AccountID: accountID.ToRaw()})
+	if err != nil {
+		return tlb.ShardAccount{}, err
+	}
+	var shardAccount tlb.ShardAccount
+	shardAccount.LastTransLt = uint64(res.LastTransactionLt)
+	switch res.Status {
+	case "nonexist":
+		shardAccount.Account.SumType = "AccountNone"
+	case "uninit":
+		shardAccount.Account.SumType = "Account"
+		shardAccount.Account.Account.Addr = accountID.ToMsgAddress()
+		shardAccount.Account.Account.Storage.Balance.Grams = tlb.Grams(res.Balance)
+		shardAccount.Account.Account.Storage.State.SumType = "AccountUninit"
+	case "active":
+		shardAccount.Account.SumType = "Account"
+		shardAccount.Account.Account.Addr = accountID.ToMsgAddress()
+		shardAccount.Account.Account.Storage.Balance.Grams = tlb.Grams(res.Balance)
+		shardAccount.Account.Account.Storage.State.SumType = "AccountActive"
+		shardAccount.Account.Account.Storage.State.AccountActive.StateInit.Code.Exists = len(res.Code.Value) > 0
+		shardAccount.Account.Account.Storage.State.AccountActive.StateInit.Data.Exists = len(res.Data.Value) > 0
+	case "frozen":
+
+	}
+	return shardAccount, nil
+}
