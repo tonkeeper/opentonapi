483cdd1f9bdd7b3955214596edad8eb7c4780ea8
aleksej.paschenko <aleksej.paschenko@gmail.com>
2023-10-04 16:46:33 +0300

    Fix sources.Tracer sending the same trace multiple times

--- pkg/pusher/sources/tracer.go
+++ pkg/pusher/sources/tracer.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"sync"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/tonkeeper/opentonapi/pkg/cache"
 	"github.com/tonkeeper/opentonapi/pkg/core"
 	"github.com/tonkeeper/tongo"
 	"go.uber.org/zap"
@@ -33,11 +35,25 @@ type storage interface {
 	GetTrace(ctx context.Context, hash tongo.Bits256) (*core.Trace, error)
 }
 
+type dispatcher interface {
+	dispatch(accountIDs []tongo.AccountID, event []byte)
+	registerSubscriber(fn DeliveryFn, options SubscribeToTraceOptions) CancelFn
+}
+
 type Tracer struct {
 	logger     *zap.Logger
 	storage    storage
-	dispatcher *TraceDispatcher
+	dispatcher dispatcher
 	source     TransactionSource
+
+	// mu protects traceCache.
+	// Tracer usually receives multiple tx hashes that are parts of the same trace,
+	// and we want to avoid sending the same trace to subscribers multiple times.
+	// We use a cache to keep track of already sent traces.
+	// "cache.Cache" doesn't provide a single atomic operation to check and set a value if it hasn't been set yet,
+	// so we use a mutex to serialize access to the cache.
+	mu         sync.Mutex
+	traceCache cache.Cache[string, struct{}]
 }
 
 func NewTracer(logger *zap.Logger, storage storage, source TransactionSource) *Tracer {
@@ -46,6 +62,7 @@ func NewTracer(logger *zap.Logger, storage storage, source TransactionSource) *T
 		storage:    storage,
 		source:     source,
 		dispatcher: NewTraceDispatcher(logger),
+		traceCache: cache.NewLRUCache[string, struct{}](10000, "tracer_trace_cache"),
 	}
 }
 
@@ -56,7 +73,7 @@ func (t *Tracer) SubscribeToTraces(ctx context.Context, deliveryFn DeliveryFn, o
 		zap.Bool("all-accounts", opts.AllAccounts),
 		zap.Stringers("accounts", opts.Accounts))
 
-	return t.dispatcher.RegisterSubscriber(deliveryFn, opts)
+	return t.dispatcher.registerSubscriber(deliveryFn, opts)
 }
 
 func (t *Tracer) Run(ctx context.Context) {
@@ -102,12 +119,28 @@ func (t *Tracer) Run(ctx context.Context) {
 	wg.Wait()
 }
 
+// putTraceInCache returns true if the trace was not in the cache before.
+func (t *Tracer) putTraceInCache(hash string) (success bool) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	if _, ok := t.traceCache.Get(hash); ok {
+		return false
+	}
+	t.traceCache.Set(hash, struct{}{}, cache.WithExpiration(10*time.Minute))
+	return true
+}
+
 func (t *Tracer) dispatch(trace *core.Trace) {
 	if trace.InProgress() {
 		traceNumber.With(map[string]string{"completeness": "inProgress"}).Inc()
 		return
 	}
 	traceNumber.With(map[string]string{"completeness": "completed"}).Inc()
+
+	if !t.putTraceInCache(trace.Hash.Hex()) {
+		return
+	}
 
 	accounts := make(map[tongo.AccountID]struct{})
 	queue := []*core.Trace{trace}
