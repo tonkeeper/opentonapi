d870718d43b022814ff0cb80fc1ada3e1ec4d3f3
Denis Subbotin <denis@subbot.in>
2023-11-01 09:15:17 +0300

    hardcode tonstakers pool

--- pkg/references/tf.go
+++ pkg/references/tf.go
@@ -1,6 +1,9 @@
 package references
 
-import "github.com/tonkeeper/tongo"
+import (
+	"github.com/tonkeeper/tongo"
+	"github.com/tonkeeper/tongo/ton"
+)
 
 const TFPoolImplementationsName = "TON Nominators"
 const TFPoolImplementationsURL = "https://tonvalidators.org/"
@@ -8,7 +11,7 @@ const TFPoolImplementationsURL = "https://tonvalidators.org/"
 // validated nominators contract code hash
 var TFPoolCodeHash = tongo.MustParseHash("9A3EC14BC098F6B44064C305222CAEA2800F17DDA85EE6A8198A7095EDE10DCF")
 
-var TonstakersAccountPool = tongo.MustParseAccountID("EQCkWxfyhAkim3g2DjKQQg8T5P4g-Q1-K_jErGcDJZ4i-vqR")
+var TonstakersAccountPool = ton.MustParseAccountID("EQCkWxfyhAkim3g2DjKQQg8T5P4g-Q1-K_jErGcDJZ4i-vqR")
 
 const TonstakersImplementationsName = "Tonstakers"
 const LiquidImplementationsUrl = "https://tonstakers.com/"
@@ -16,3 +19,4 @@ const LiquidImplementationsUrl = "https://tonstakers.com/"
 var TonstakersSocialLinks = []string{"https://t.me/thetonstakers", "https://twitter.com/tonstakers"}
 
 var TFLiquidPoolCodeHash = tongo.MustParseHash("e8eeae986d782a96eef432c45edd1f0a84b943416ccd8caa42118c0dc7b1d34a")
+var TFLiquidPool = ton.MustParseAccountID("0:a45b17f28409229b78360e3290420f13e4fe20f90d7e2bf8c4ac6703259e22fa")
