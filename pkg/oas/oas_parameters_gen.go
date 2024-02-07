f67fa41f74657143faa587f58070fb086fee787a
Denis Subbotin <denis@subbot.in>
2024-02-07 17:11:01 +0300

    decrease limit

--- pkg/oas/oas_parameters_gen.go
+++ pkg/oas/oas_parameters_gen.go
@@ -1764,7 +1764,7 @@ func decodeGetAccountEventsParams(args [1]string, argsEscaped bool, r *http.Requ
 					MinSet:        true,
 					Min:           1,
 					MaxSet:        true,
-					Max:           1000,
+					Max:           100,
 					MinExclusive:  false,
 					MaxExclusive:  false,
 					MultipleOfSet: false,
