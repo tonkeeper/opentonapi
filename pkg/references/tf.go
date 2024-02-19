f92fb67752b159d425aed067655edb9f10e91e27
Denis Subbotin <denis@subbot.in>
2024-02-19 19:20:23 +0300

    change reference code

--- pkg/references/tf.go
+++ pkg/references/tf.go
@@ -19,5 +19,5 @@ const LiquidImplementationsUrl = "https://tonstakers.com/"
 var TonstakersSocialLinks = []string{"https://t.me/thetonstakers", "https://twitter.com/tonstakers"}
 var TonstakersAccountPool = ton.MustParseAccountID("EQCkWxfyhAkim3g2DjKQQg8T5P4g-Q1-K_jErGcDJZ4i-vqR")
 
-var TFLiquidPoolCodeHash = tongo.MustParseHash("e8eeae986d782a96eef432c45edd1f0a84b943416ccd8caa42118c0dc7b1d34a")
+var TFLiquidPoolCodeHash = tongo.MustParseHash("192535677eed65c20ac387efe4dd7415ad9ebb9349103e87c60e592538c9dcf3")
 var TFLiquidPool = ton.MustParseAccountID("0:a45b17f28409229b78360e3290420f13e4fe20f90d7e2bf8c4ac6703259e22fa")
