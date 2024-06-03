9af5fb8295f6d5fabf2c6f7f1841a77d0ad59904
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2024-06-03 10:43:09 +0800

    remove the nil pointer from the spam filter

--- pkg/spam/spam.go
+++ pkg/spam/spam.go
@@ -1,26 +1,39 @@
 package spam
 
 import (
+	"sync"
+
 	rules "github.com/tonkeeper/scam_backoffice_rules"
 	"github.com/tonkeeper/tongo"
 )
 
 type SpamFilter struct {
-	Rules          rules.Rules
-	jettonVerifier *rules.JettonVerifier
+	mu                     sync.RWMutex
+	Rules                  rules.Rules
+	blacklistedCollections map[tongo.AccountID]bool
+	jettonVerifier         *rules.JettonVerifier
 }
 
 func NewSpamFilter() *SpamFilter {
 	return &SpamFilter{
-		Rules:          rules.GetDefaultRules(),
-		jettonVerifier: rules.NewJettonVerifier(),
+		Rules:                  rules.GetDefaultRules(),
+		jettonVerifier:         rules.NewJettonVerifier(),
+		blacklistedCollections: make(map[tongo.AccountID]bool),
 	}
 }
 
 func (s *SpamFilter) GetRules() rules.Rules {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
 	return s.Rules
 }
 
 func (s *SpamFilter) IsJettonBlacklisted(address tongo.AccountID, symbol string) bool {
 	return s.jettonVerifier.IsBlacklisted(address, symbol)
+}
+
+func (s *SpamFilter) IsCollectionBlacklisted(address tongo.AccountID) bool {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.blacklistedCollections[address]
 }
