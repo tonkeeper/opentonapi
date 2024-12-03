a26c94555efe04741283270174eb184cc31aa54f
Alexey Kostenko <a.kos@tonkeeper.com>
2024-12-03 23:45:49 +0300

    copy via go:generate

--- gen.go
+++ gen.go
@@ -2,5 +2,5 @@ package opentonapi
 
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen -clean -config ogen.yaml -package oas -target pkg/oas api/openapi.yml
 //go:generate go run api/jsonify.go api/openapi.yml api/openapi.json
-//go:generate bash -c "cp api/openapi.yml pkg/api/openapi/openapi.yml"
-//go:generate bash -c "cp api/openapi.json pkg/api/openapi/openapi.json"
+//go:generate cp api/openapi.yml pkg/api/openapi/openapi.yml
+//go:generate cp api/openapi.json pkg/api/openapi/openapi.json
