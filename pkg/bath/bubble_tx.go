3682c477d7a5592e5e2d9250b6df0e4abceff5ee
Denis Subbotin <denis@subbot.in>
2025-02-11 12:09:31 +0300

    minor fix

--- pkg/bath/bubble_tx.go
+++ pkg/bath/bubble_tx.go
@@ -52,7 +52,7 @@ func (b BubbleTx) ToAction() *Action {
 		}
 		return nil
 	}
-	if b.opCode != nil && (*b.opCode != 0 && !b.operation(abi.EncryptedTextCommentMsgOp)) && b.accountWasActiveAtComputingTime && !b.account.Is(abi.Wallet) && len(b.inputExtraAmount) == 0 {
+	if b.opCode != nil && (*b.opCode != 0 && !b.operation(abi.EncryptedTextCommentMsgOp)) && b.accountWasActiveAtComputingTime && !b.account.Is(abi.Wallet) && b.inputAmount != 0 {
 		operation := fmt.Sprintf("0x%08x", *b.opCode)
 		payload := ""
 		if b.decodedBody != nil {
