b2aeafe4fb949e56afb0cafb754583d94ad04f81
Alexey Kostenko <a.kos@tonkeeper.com>
2025-03-24 17:37:20 +0300

    get extra currency info

--- pkg/api/extra_currency_handlers.go
+++ pkg/api/extra_currency_handlers.go
@@ -2,10 +2,17 @@ package api
 
 import (
 	"context"
+	"github.com/tonkeeper/opentonapi/pkg/references"
 
 	"github.com/tonkeeper/opentonapi/pkg/oas"
 )
 
 func (h *Handler) GetExtraCurrencyInfo(ctx context.Context, params oas.GetExtraCurrencyInfoParams) (*oas.EcPreview, error) {
-	return &oas.EcPreview{}, nil
+	meta := references.GetExtraCurrencyMeta(params.ID)
+	return &oas.EcPreview{
+		ID:       params.ID,
+		Symbol:   meta.Symbol,
+		Decimals: meta.Decimals,
+		Image:    meta.Image,
+	}, nil
 }
