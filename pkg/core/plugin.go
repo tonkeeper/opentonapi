c2069a98664d91409929c2bbd2ac10ac7ddf3b11
Alexey Kostenko <a.kos@tonkeeper.com>
2025-06-04 14:41:08 +0300

    update plugins

--- pkg/core/plugin.go
+++ pkg/core/plugin.go
@@ -1,10 +1,12 @@
 package core
 
 import (
+	"github.com/tonkeeper/tongo/tlb"
 	"github.com/tonkeeper/tongo/ton"
 )
 
 type Plugin struct {
 	AccountID ton.AccountID
+	Status    tlb.AccountStatus
 	Type      string
 }
