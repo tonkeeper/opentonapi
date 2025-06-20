60a1ccbfa5402c6dd404a4c3eef3c37dd40e8d6f
Alexey Kostenko <a.kos@tonkeeper.com>
2025-06-20 18:27:28 +0300

    subscription straw draft

--- pkg/bath/subscriptions.go
+++ pkg/bath/subscriptions.go
@@ -1,12 +1,14 @@
 package bath
 
 import (
+	"github.com/tonkeeper/opentonapi/pkg/core"
 	"github.com/tonkeeper/tongo/abi"
+	"math/big"
 )
 
 type BubbleSubscription struct {
-	Subscription, Subscriber, Beneficiary Account
-	Amount                                int64
+	Subscription, Subscriber, Beneficiary Account    // TODO: Beneficiary not valid name for subscription V2 (recipient)
+	Amount                                core.Price // TODO: replace with price
 	Success                               bool
 	First                                 bool
 }
@@ -17,12 +19,15 @@ type BubbleUnSubscription struct {
 }
 
 var InitialSubscriptionStraw = Straw[BubbleSubscription]{
-	CheckFuncs: []bubbleCheck{IsTx, HasOperation(abi.PaymentRequestResponseMsgOp), AmountInterval(1, 1<<62)},
+	CheckFuncs: []bubbleCheck{IsTx, HasInterface(abi.SubscriptionV1), HasOperation(abi.PaymentRequestResponseMsgOp), AmountInterval(1, 1<<62)}, // TODO: equal to abi.SubscriptionV2PaymentConfirmedMsgOp
 	Builder: func(newAction *BubbleSubscription, bubble *Bubble) error {
 		tx := bubble.Info.(BubbleTx)
 		newAction.Subscriber = *tx.inputFrom
 		newAction.Subscription = tx.account
-		newAction.Amount = tx.inputAmount
+		newAction.Amount = core.Price{ // only ton payments supported by Subscription V1
+			Type:   core.CurrencyTON,
+			Amount: *big.NewInt(tx.inputAmount),
+		}
 		newAction.First = len(tx.init) != 0
 		return nil
 	},
@@ -36,8 +41,33 @@ var InitialSubscriptionStraw = Straw[BubbleSubscription]{
 	},
 }
 
+var InitialSubscriptionV2Straw = Straw[BubbleSubscription]{
+	CheckFuncs: []bubbleCheck{IsTx, HasInterface(abi.SubscriptionV2), HasOperation(abi.SubscriptionV2PaymentConfirmedMsgOp), AmountInterval(1, 1<<62)}, // TODO: equal to abi.PaymentRequestResponseMsgOp
+	Builder: func(newAction *BubbleSubscription, bubble *Bubble) error {
+		tx := bubble.Info.(BubbleTx)
+		newAction.Subscriber = *tx.inputFrom
+		newAction.Subscription = tx.account
+		newAction.Amount = core.Price{ // only ton payments supported by Subscription V2
+			Type:   core.CurrencyTON,
+			Amount: *big.NewInt(tx.inputAmount),
+		}
+		newAction.First = len(tx.init) != 0
+		return nil
+	},
+	// TODO: how to check custom out msg?
+	SingleChild: &Straw[BubbleSubscription]{
+		CheckFuncs: []bubbleCheck{IsTx, Or(HasOperation(abi.TextCommentMsgOp), HasEmptyBody)},
+		Builder: func(newAction *BubbleSubscription, bubble *Bubble) error {
+			newAction.Success = true // subscription V2 uses non-bounce flag
+			newAction.Beneficiary = bubble.Info.(BubbleTx).account
+			return nil
+		},
+	},
+}
+
+// TODO: conflicts with ExtendedSubscriptionW5Straw
 var ExtendedSubscriptionStraw = Straw[BubbleSubscription]{
-	CheckFuncs: []bubbleCheck{IsTx, HasInterface(abi.SubscriptionV1)},
+	CheckFuncs: []bubbleCheck{IsTx, Or(HasInterface(abi.SubscriptionV1), HasInterface(abi.SubscriptionV2))}, // same behavior sub v1 and v2 for wallet V4
 	Builder: func(newAction *BubbleSubscription, bubble *Bubble) error {
 		tx := bubble.Info.(BubbleTx)
 		newAction.Subscription = tx.account
@@ -50,7 +80,58 @@ var ExtendedSubscriptionStraw = Straw[BubbleSubscription]{
 			request := tx.decodedBody.Value.(abi.PaymentRequestMsgBody)
 			newAction.Subscriber = tx.account
 			newAction.Success = tx.success
-			newAction.Amount = int64(request.Amount.Grams)
+			newAction.Amount = core.Price{ // only ton payments supported by SubscriptionV1
+				Type:   core.CurrencyTON,
+				Amount: *big.NewInt(int64(request.Amount.Grams)),
+			}
+			return nil
+		},
+		SingleChild: &Straw[BubbleSubscription]{
+			Optional:   true,
+			CheckFuncs: []bubbleCheck{Is(BubbleSubscription{})},
+			Builder: func(newAction *BubbleSubscription, bubble *Bubble) error {
+				sub := bubble.Info.(BubbleSubscription)
+				newAction.First = false
+				newAction.Beneficiary = sub.Beneficiary
+				newAction.Success = true
+				return nil
+			},
+		},
+	},
+}
+
+// TODO: deploy of subscription also may be first payment but has another pattern
+
+var ExtendedSubscriptionW5Straw = Straw[BubbleSubscription]{
+	CheckFuncs: []bubbleCheck{IsTx, HasInterface(abi.SubscriptionV2)},
+	Builder: func(newAction *BubbleSubscription, bubble *Bubble) error {
+		tx := bubble.Info.(BubbleTx)
+		newAction.Subscription = tx.account
+		return nil
+	},
+	SingleChild: &Straw[BubbleSubscription]{
+		CheckFuncs: []bubbleCheck{IsTx, HasOperation(abi.WalletExtensionActionV5R1MsgOp), // TODO: check wallet interface?
+			func(bubble *Bubble) bool {
+				tx := bubble.Info.(BubbleTx)
+				request := tx.decodedBody.Value.(abi.WalletExtensionActionV5R1MsgBody)
+				if request.Actions == nil {
+					return false
+				}
+				if len([]abi.W5SendMessageAction(*request.Actions)) != 1 {
+					return false
+				}
+				return true
+			}},
+		Builder: func(newAction *BubbleSubscription, bubble *Bubble) error {
+			tx := bubble.Info.(BubbleTx)
+			request := tx.decodedBody.Value.(abi.WalletExtensionActionV5R1MsgBody)
+			newAction.Subscriber = tx.account
+			newAction.Success = tx.success // TODO: if success but no out msgs? maybe check wallet interface?
+			walletAction := []abi.W5SendMessageAction(*request.Actions)[0]
+			newAction.Amount = core.Price{ // only ton payments supported by SubscriptionV2
+				Type:   core.CurrencyTON,
+				Amount: *big.NewInt(int64(walletAction.Msg.MessageInternal.Value.Grams)),
+			}
 			return nil
 		},
 		SingleChild: &Straw[BubbleSubscription]{
@@ -73,8 +154,8 @@ func (b BubbleSubscription) ToAction() (action *Action) {
 			Subscription: b.Subscription.Address,
 			Subscriber:   b.Subscriber.Address,
 			Beneficiary:  b.Beneficiary.Address,
-			Amount:       b.Amount,
-			First:        b.First,
+			// Amount:       b.Amount, // TODO: replace with Price
+			First: b.First,
 		},
 		Success: true,
 		Type:    Subscription,
