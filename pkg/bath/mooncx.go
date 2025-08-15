ab0ff0a1f560b8246615a9c8159f47312439d18a
Pavel Levchenko <pashalevchenko703@gmail.com>
2025-08-15 10:26:25 +0600

    mooncx swap straws

---
+++ pkg/bath/mooncx.go
@@ -0,0 +1,65 @@
+package bath
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/tonkeeper/tongo/abi"
+)
+
+var MooncxSwapStraw = Straw[BubbleJettonSwap]{
+	CheckFuncs: []bubbleCheck{IsTx, HasOperation(abi.MoonSwapMsgOp), HasInterface(abi.MoonPool)},
+	Builder: func(newAction *BubbleJettonSwap, bubble *Bubble) error {
+		tx := bubble.Info.(BubbleTx)
+		newAction.Dex = Mooncx
+		newAction.UserWallet = tx.inputFrom.Address
+		newAction.Router = tx.account.Address
+		body, ok := tx.decodedBody.Value.(abi.MoonSwapMsgBody)
+		if !ok {
+			return fmt.Errorf("body is not a mooncx swap body")
+		}
+		amount := big.Int(body.Amount)
+		newAction.In.Amount = amount
+		newAction.In.IsTon = true
+		return nil
+	},
+	SingleChild: &Straw[BubbleJettonSwap]{
+		CheckFuncs: []bubbleCheck{IsJettonTransfer},
+		Builder: func(newAction *BubbleJettonSwap, bubble *Bubble) error {
+			tx := bubble.Info.(BubbleJettonTransfer)
+			newAction.Out.JettonMaster = tx.master
+			newAction.Out.JettonWallet = tx.senderWallet
+			amount := big.Int(tx.amount)
+			newAction.Out.Amount = amount
+			newAction.Success = tx.success
+			return nil
+		},
+	},
+}
+
+var MooncxSwapStrawReverse = Straw[BubbleJettonSwap]{
+	CheckFuncs: []bubbleCheck{IsJettonTransfer},
+	Builder: func(newAction *BubbleJettonSwap, bubble *Bubble) error {
+		tx := bubble.Info.(BubbleJettonTransfer)
+		newAction.Dex = Mooncx
+		newAction.UserWallet = tx.senderWallet
+		newAction.Router = tx.recipient.Address
+		newAction.In.JettonMaster = tx.master
+		newAction.In.JettonWallet = tx.senderWallet
+		amount := big.Int(tx.amount)
+		newAction.In.Amount = amount
+		body := tx.payload.Value.(abi.MoonSwapJettonPayload)
+		newAction.Out.Amount = big.Int(body.SwapParams.MinOut)
+		return nil
+	},
+	SingleChild: &Straw[BubbleJettonSwap]{
+		CheckFuncs: []bubbleCheck{IsTx, HasOperation(abi.MoonSwapSucceedJettonOp), HasInterface(abi.Wallet)},
+		Builder: func(newAction *BubbleJettonSwap, bubble *Bubble) error {
+			tx := bubble.Info.(BubbleTx)
+			newAction.UserWallet = tx.account.Address
+			newAction.Out.IsTon = true
+			newAction.Success = tx.success
+			return nil
+		},
+	},
+}
