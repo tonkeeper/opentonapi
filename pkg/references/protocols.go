91aa5891c30eb7f8f80e203e741517aca7c68f85
Pavel Levchenko <pashalevchenko703@gmail.com>
2025-11-11 11:08:44 +0300

    implement dedust liquidity deposit straw

--- pkg/references/protocols.go
+++ pkg/references/protocols.go
@@ -8,10 +8,11 @@ const (
 )
 
 var (
-	EthenaImage = "https://ethena.fi/shared/usde.png"
-	BidaskImage = "https://bidask.finance/assets/landing/bidask-logo.webp"
-	StonfiImage = "https://static.ston.fi/favicon/android-chrome-192x192.png"
-	MooncxImage = "https://moon.cx/assets/logoMoon.svg"
+	EthenaImage   = "https://ethena.fi/shared/usde.png"
+	BidaskImage   = "https://bidask.finance/assets/landing/bidask-logo.webp"
+	StonfiImage   = "https://static.ston.fi/favicon/android-chrome-192x192.png"
+	DedustImage   = "https://ton.app/media/36676ef1-0094-4238-98fb-d65c64a51323.jpg"
+	MooncxImage   = "https://moon.cx/assets/logoMoon.svg"
 	ToncoImage    = "https://ton.app/media/1f913e65-9c32-433e-a0a3-a7c5ccf46ad5.png"
 	AffluentImage = "https://ton.app/media/71e5021a-77ab-4c48-8ef3-35e9c67701b0.png"
 )
