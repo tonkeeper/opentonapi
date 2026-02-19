55b2262cd7735c32bf52f542dc01abd760b19ce0
Mikhail Alpinskiy <m.alpinskiy@top.team>
2026-02-19 12:24:36 +0300

    Gasless hotfix

--- pkg/api/gasless_handlers.go
+++ pkg/api/gasless_handlers.go
@@ -23,7 +23,26 @@ func (h *Handler) GaslessConfig(ctx context.Context) (*oas.GaslessConfig, error)
 	config, err := h.gasless.Config(ctx)
 	if err != nil {
 		h.logger.Warn("failed to get gasless config", zap.Error(err))
-		return nil, toError(http.StatusInternalServerError, fmt.Errorf("failed to get gasless config"))
+		// TODO:
+		// return nil, toError(http.StatusInternalServerError, fmt.Errorf("failed to get gasless config"))
+		return &oas.GaslessConfig{
+			RelayAddress: "0:dfbd5be8497fdc0c9fcbdfc676864840ddf8ad6423d6d5657d9b0e8270d6c8ac",
+			GasJettons: []oas.GaslessConfigGasJettonsItem{
+				{MasterID: "0:3702c84f115972f3043a9998a772b282fc290948a5eaaa3ca0d1532c56317f08"},
+				{MasterID: "0:78cd9bac1ec6d4daf5533ea8e19689083a8899844742313ef4dc2584ce14cea3"},
+				{MasterID: "0:589d4ac897006b5aaa7fae5f95c5e481bd34765664df0b831a9d0eb9ee7fc150"},
+				{MasterID: "0:b113a994b5024a16719f69139328eb759596c38a25f59028b146fecdc3621dfe"},
+				{MasterID: "0:e8976a15a660739c02fabd0a45e75416de9d0f295eda838544b5ec7cfcc78c1c"},
+				{MasterID: "0:ae3e6d351e576276e439e7168117fd64696fd6014cb90c77b2f2cbaacd4fcc00"},
+				{MasterID: "0:fe72f474373e97032441bdb873f9a6d3ad10bab08e6dbc7befa5e42b695f5400"},
+				{MasterID: "0:bdf3fa8098d129b54b4f73b5bac5d1e1fd91eb054169c3916dfc8ccd536d1000"},
+				{MasterID: "0:2f956143c461769579baef2e32cc2d7bc18283f40d20bb03e432cd603ac33ffc"},
+				{MasterID: "0:afc49cb8786f21c87045b19ede78fc6b46c51048513f8e9a6d44060199c1bf0c"},
+				{MasterID: "0:09f2e59dec406ab26a5259a45d7ff23ef11f3e5c7c21de0b0d2a1cbe52b76b3d"},
+				{MasterID: "0:78db4c90b19a1b19ccb45580df48a1e91b6410970fa3d5ffed3eed49e3cf08ff"},
+				{MasterID: "0:f4bdd480fcd79d47dbaf6e037d1229115feb2e7ac0f119e160ebd5d031abdf2e"},
+			},
+		}, nil
 	}
 	o := &oas.GaslessConfig{
 		GasJettons:   make([]oas.GaslessConfigGasJettonsItem, 0, len(config.SupportedJettons)),
