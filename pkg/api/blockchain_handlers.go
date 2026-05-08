230c480a69d90668b9fed161138e328379b7fd35
Alexander Hasselbach <alex@hsslb.ch>
2026-05-08 17:28:41 +0300

    ignore broken config params

--- pkg/api/blockchain_handlers.go
+++ pkg/api/blockchain_handlers.go
@@ -371,7 +371,7 @@ func (h *Handler) GetBlockchainValidators(ctx context.Context) (*oas.Validators,
 	if err != nil {
 		return nil, toError(http.StatusInternalServerError, err)
 	}
-	config, err := ton.ConvertBlockchainConfigStrict(rawConfig)
+	config, _, err := ton.ConvertBlockchainConfig(rawConfig, true)
 	if err != nil {
 		return nil, toError(http.StatusInternalServerError, err)
 	}
