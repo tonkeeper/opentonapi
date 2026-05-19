fac24b6ff562c11022a787880e53ccee054e99f2
Mikhail Alpinskiy <ama1185@ya.ru>
2026-05-19 18:40:01 +0300

    Fixed limit
    /v2/accounts/{account_id}/jettons

--- pkg/api/jetton_handlers.go
+++ pkg/api/jetton_handlers.go
@@ -21,7 +21,7 @@ func (h *Handler) GetAccountJettonsBalances(ctx context.Context, params oas.GetA
 	if err != nil {
 		return nil, toError(http.StatusBadRequest, err)
 	}
-	wallets, err := h.storage.GetJettonWalletsByOwnerAddress(ctx, account.ID, nil, true, slices.Contains(params.SupportedExtensions, "custom_payload"), params.Offset.Value, params.Offset.Value)
+	wallets, err := h.storage.GetJettonWalletsByOwnerAddress(ctx, account.ID, nil, true, slices.Contains(params.SupportedExtensions, "custom_payload"), params.Limit.Value, params.Offset.Value)
 	if errors.Is(err, core.ErrEntityNotFound) {
 		return &oas.JettonsBalances{}, nil
 	}
